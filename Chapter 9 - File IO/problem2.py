3f3761c46d844ef59d36124bf081a48d656336bd
unknown <bhuwanpulamimagar15220@gmail.com>
2026-04-16 09:34:49 +0545

    initial commit

--- Chapter 9 - File IO/problem2.py
+++ Chapter 9 - File IO/problem2.py
@@ -2,3 +2,27 @@
 # file ‘Hi-score.txt’ which is either blank or contains the previous Hi-score. You need to write a program to update 
 # the Hiscore whenever the game() function breaks the Hi-score.
 
+import random
+
+def game():
+    score = random.randint(1, 100)
+    return score
+
+score = game()
+
+try:
+    with open(r"D:\ML with Python\Python\Chapter 9 - File IO\Hi_score.txt", "r") as f:
+        hi_score_str = f.read()
+        if hi_score_str == "":
+            hi_score = 0
+        else:
+            hi_score = int(hi_score_str)
+except FileNotFoundError:
+    hi_score = 0
+
+if score > hi_score:
+    print(f"Congratulations! You broke the high score. New High Score is {score}!")
+    with open(r"D:\ML with Python\Python\Chapter 9 - File IO\Hi_score.txt", "w") as f:
+        f.write(str(score))
+else:
+    print(f"The current high score is {hi_score}. Try again to beat it!")   
