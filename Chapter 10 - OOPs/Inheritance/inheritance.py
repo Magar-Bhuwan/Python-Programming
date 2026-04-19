67666e4521f1f80f8c7976c26af2632166bd286f
bhuwanpulamimagar15220@gmail.com <bhuwanpulamimagar15220@gmail.com>
2026-04-19 18:27:47 +0545

    Merge branch 'main' of github.com:Magar-Bhuwan/Python-Programming into feature/dev

--- Chapter 10 - OOPs/Inheritance/inheritance.py
+++ Chapter 10 - OOPs/Inheritance/inheritance.py
@@ -6,13 +6,15 @@
 # class child_class(parent_class):
 #     # body of child class
 
-class Animal:
+# parent class
+class Animal:                   
     def __init__(self, name):
         self.name = name
     
     def eat(self):
         print(f"{self.name} is eating. ")
 
+# child class
 class Dog(Animal):
     def bark(self):
         print(f"{self.name} is barking. ")
