46465e77148c01fb9b2e35433b1446d31bb4ee18
bhuwanpulamimagar15220@gmail.com <bhuwanpulamimagar15220@gmail.com>
2026-04-26 19:45:15 +0545

    initial commit

--- cmddd.py
+++ cmddd.py
@@ -1,20 +1,27 @@
 
-current_price = float(input("Enter your current price: "))
-target_price = float(input("Enter your target price: "))
-daily_increase_rate = float(input("Enter your daily increase percentage: "))
-rate = daily_increase_rate / 100
-print("\n")
+# current_price = float(input("Enter your current price: "))
+# target_price = float(input("Enter your target price: "))
+# daily_increase_rate = float(input("Enter your daily increase percentage: "))
+# rate = daily_increase_rate / 100
+# print("\n")
 
-days = 0
-starting_price = current_price
+# days = 0
+# starting_price = current_price
 
-while current_price <= target_price:
-    current_price *= (1 + rate)
-    days += 1
+# while current_price <= target_price:
+#     current_price *= (1 + rate)
+#     days += 1
 
-print(f"Current price: Rs.{starting_price:,.2f}")        # i want to put the user entered current price here
-print(f"Target price: Rs.{target_price:,.2f}")
-print(f"Daily increase: {daily_increase_rate}%")
-print(f"Days to reach target price: {days} days")
-print(f"Final price after {days} days: Rs.{current_price:,.2f}")
-print(f"After 7% cut off you will get total: {current_price -(current_price*0.07):,.2f}")
+# print(f"Current price: Rs.{starting_price:,.2f}")        # i want to put the user entered current price here
+# print(f"Target price: Rs.{target_price:,.2f}")
+# print(f"Daily increase: {daily_increase_rate}%")
+# print(f"Days to reach target price: {days} days")
+# print(f"Final price after {days} days: Rs.{current_price:,.2f}")
+# print(f"After 7% cut off you will get total: {current_price -(current_price*0.07):,.2f}")
+
+
+# Find all python errors
+import re
+for i in dir(__builtins__):
+    if re.match(r'^[A-Z]',i):
+        print(i)
